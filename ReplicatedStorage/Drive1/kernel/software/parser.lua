530f29d871e2c30fa72ef1e73a9965fbdf76c468
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-03-09 16:02:10 +1300

    Create parser.lua

---
+++ ReplicatedStorage/Drive1/kernel/software/parser.lua
@@ -0,0 +1,32 @@
+return { -- adapted from sword admin commands by me :)
+	find_in_bounds = function(str, match)
+		if string.find(str, match) == nil then return str end
+		local _str = str:sub(string.find(str, match) + match:len(), str:len())
+		local _str = _str:sub(1, string.find(_str, match) - match:len())
+		return _str
+	end,
+
+	get_command = function(str)
+		return string.split(str, " ")[1]:sub(1, str:len()) or str:sub(1, str:len())
+	end,
+
+	get_time_period = {
+		periods = {
+			["m"] = {"month" , 2628288},
+			["d"] = {"day", 86400},
+			["h"] = {"hour", 3600},
+			["s"] = {"second", 2}
+		},
+		parse = function(str)
+			
+		end,
+	},
+	
+	array_as_string = function(arr)
+		local str = ""
+		for _, v in pairs (arr) do
+			str ..= "\n" .. tostring(v) 	
+		end
+		return str
+	end,
+}
