9288e8ac66c401334d10b91e152928512ef73785
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-03-22 22:05:48 +1300

    Update handler.lua

--- ReplicatedStorage/Drive1/dev_env/UI/LOG_IN(screengui)/Background(frame)/handler.lua
+++ ReplicatedStorage/Drive1/dev_env/UI/LOG_IN(screengui)/Background(frame)/handler.lua
@@ -1,11 +1,19 @@
-local drive = game.ReplicatedStorage["Drive1"]
+local drive = game.ReplicatedStorage["Drive01"]
 local dev_env = drive.dev_env.Sword
 
-script.Parent.intro.Text = "welcome, " .. drive.user.environment.Value
-
 script.Parent.moveon.MouseButton1Click:Connect(function()
-	if script.Parent.passprompt.Text == drive.user.admin.info.password.Value then 
-		dev_env.UI.Main.curuser.Value = "admin"
+	local username = script.Parent.usrprompt.Text
+	
+	if script.Parent.passprompt.Text == drive.user[username].info.password.Value then 
+		if drive.user.environment.Value ~= username then 
+			local apps = game.ReplicatedStorage.applications_backup:Clone()
+			dev_env.UI.Main.applications:Destroy()
+			apps.Name = "applications"
+			apps.Parent = dev_env.UI.Main
+		end
+		
+		drive.user.environment.Value = username
+		dev_env.UI.Main.curuser.Value = username
 		dev_env.UI.Main.Parent = game.Players.LocalPlayer.PlayerGui
 		script.Parent.Parent.Parent = dev_env.UI 
 	end
