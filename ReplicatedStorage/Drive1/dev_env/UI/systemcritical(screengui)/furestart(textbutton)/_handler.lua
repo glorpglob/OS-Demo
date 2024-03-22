c9d3baafe8362d0d3404095f8123e3133ecee1be
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-03-22 22:02:42 +1300

    Update _handler.lua

--- ReplicatedStorage/Drive1/dev_env/UI/systemcritical(screengui)/furestart(textbutton)/_handler.lua
+++ ReplicatedStorage/Drive1/dev_env/UI/systemcritical(screengui)/furestart(textbutton)/_handler.lua
@@ -1,5 +1,5 @@
 script.Parent.MouseButton1Click:Connect(function() 
-	local drive = game.ReplicatedStorage["Drive1"]
+	local drive = game.ReplicatedStorage["Drive01"]
 	local cmds = require(drive.etc.user.ENV_PATH)
 	
 	cmds["system"][1]({"restart"})
